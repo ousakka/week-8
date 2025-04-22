92e439b0bdad42935403d1cee61068a844a20b53
ousakka <oumaima.sakka.os@gmail.com>
2025-04-22 18:22:03 +0200

    creating a loop function

--- exercices/exercice 1/script.js
+++ exercices/exercice 1/script.js
@@ -1,6 +1,5 @@
+const shops = ["Lidl", "føtex", "Rema 1000"];
 
-const shops = ["Lidl", "føtex" , "Rema 1000"]
- 
-for (let i=0; i<= (shops.length-1);i++){
-    console.log(shops[i]);
+for (let i = 0; i <= shops.length - 1; i++) {
+  console.log(shops[i]);
 }
